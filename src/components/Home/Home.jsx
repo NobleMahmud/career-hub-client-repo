71608fc0c65ccf29f1ff6c6c0cf8a990c099f304
NobleMahmud <mahmud.oneis1@gmail.com>
2023-11-06 02:38:43 +0600

    banner section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import animated from '../../../public/animated5.gif'
+
 
 const Home = () => {
     return (
-        <div>
-            <h3>this is home</h3>
+        <div className=' w-full bg-cover flex items-center'>
+            <img className='w-8/12' src={animated} alt="" />
+            <div className='w-4/12 space-y-3'>
+                <h3  className='lg:text-6xl md:text-3xl text-md font-bold text-blue-600'>Welcome to</h3>
+                <h3 className='md:text-4xl text-xl font-bold'><span className='text-[#545454] text-md'>CAREER-</span>HUB </h3>
+                <p className='font-semibold text-xs md:text-xl lg:text-2xl'>YOUR GATEWAY TO SUCCESS!</p>
+                
+            </div>
         </div>
     );
 };
